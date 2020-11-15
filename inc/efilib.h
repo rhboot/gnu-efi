e85b76f2b6fecdb5b9ace2d888256cd0a0b3213b
Jiaqing Zhao <jiaqing.zhao@intel.com>
2020-11-15 15:15:18 +0800

    Move EFI_SHELL_PARAMETERS_PROTOCOL related definitions to efishell.h
    As EFI_SHELL_PARAMETERS_PROTOCOL is also defined in UEFI Shell Spec,
    put it together with EFI_SHELL_PROTOCOL.
    
    Signed-off-by: Jiaqing Zhao <jiaqing.zhao@intel.com>

--- inc/efilib.h
+++ inc/efilib.h
@@ -169,6 +169,7 @@ extern EFI_GUID gEfiDebugSupportProtocolGuid;
 extern EFI_GUID SimpleTextInputExProtocol;
 
 extern EFI_GUID ShellProtocolGuid;
+extern EFI_GUID ShellParametersProtocolGuid;
 
 //
 // EFI Variable strings
